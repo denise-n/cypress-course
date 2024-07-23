c1bbac12debd10801053a769b2dab8eca171a150
Denise N <denise.ndalamba@gmail.com>
2024-07-22 22:17:54 -0400

    set baseUrl + write fundamentals page tests

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,15 @@
 describe('Fundamentals test', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/fundamentals');
+  it('Contains correct header text', () => {
+    cy.visit('/fundamentals');
     cy.get('[data-test=fundamentals-header]').contains(/Testing Fundamentals/i);
+  });
+
+  it('contains accordion that works correctly', () => {
+    cy.visit('/fundamentals');
+    cy.contains(/Your tests will exist in a/i).should('not.be.visible');
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a/i).should('be.visible');
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a/i).should('not.be.visible');
   });
 });
