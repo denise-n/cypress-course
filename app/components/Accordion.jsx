1ad01eb08b5ea0975f72d624807409b44b58ce6c
Denise N <denise.ndalamba@gmail.com>
2024-07-24 09:45:28 -0400

    add component test for Accordion component

--- app/components/Accordion.jsx
+++ app/components/Accordion.jsx
@@ -7,7 +7,10 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function ItemsAccordion({ items }) {
   return (
-    <div style={{ maxWidth: '70vw', minWidth: '50vw' }}>
+    <div
+      style={{ maxWidth: '70vw', minWidth: '50vw' }}
+      data-test="accordion-wrapper"
+    >
       {items.map((item) => {
         return (
           <Accordion data-test={`accordion-item-${item.id}`} key={item.id}>
